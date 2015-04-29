cb75d38dfa191041baae1d53801fcd6bf5eb510d
Szop-Kradziej <karolina.drabarz@gmail.com>
2015-04-29 23:07:10 +0200

    Nagłówek dla kontroler.hpp

---
+++ kontroler.hpp
@@ -0,0 +1,25 @@
+#ifndef kontroler_hpp
+#define kontroler_hpp
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <iostream>
+#include <sstream>
+#include <list>
+#include "model.hpp"
+#include "model.cpp"
+
+using namespace std;
+
+class Kontroler
+{
+	private:
+	Model *model;
+	
+	public:
+	Kontroler();
+	void petlaAplikacji();
+};
+
+#endif
