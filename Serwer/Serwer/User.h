fc22fbcd928773a121f34de3052dcfe6b2f4bc4d
Szop-Kradziej <karolina.drabarz@gmail.com>
2015-05-16 16:14:10 +0200

    Dodanie id użytkownika

--- Serwer/Serwer/User.h
+++ Serwer/Serwer/User.h
@@ -7,6 +7,9 @@ class User
 private:
 	//Unikalna nazwa uzytkownika
 	std::string nazwa;
+	
+	//Unikalny identyfikator uzytkownika
+	unsigned int id;
 
 	//Lista znajomych
 	std::vector<User*> znajomi;
