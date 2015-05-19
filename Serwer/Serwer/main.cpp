2e6fb77959d5bc13e75b285cd8d252029367c244
WiktorSzczepanski <w.i.szczepanski@gmail.com>
2015-05-19 15:50:05 +0200

    Refactor. std::cout i std::endl

--- Serwer/Serwer/main.cpp
+++ Serwer/Serwer/main.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <thread>
 
+using std::endl;
+using std::cout;
+
 bool kA = false, kB = false, dot = true;
 
 void funA()
 {
-	std::cout << "A " << std::endl;
+	cout << "A " << endl;
 	std::chrono::seconds sec(5);
 	std::this_thread::sleep_for(sec);
-	std::cout << "A2 " << std::endl;
+	cout << "A2 " << endl;
 	kA = true;
 	if (kB && kA)	dot = false;
 }
 
 void funB()
 {
-	std::cout << "B " <<std::endl;
+	cout << "B " << endl;
 	std::chrono::seconds sec(2);
 	std::this_thread::sleep_for(sec);
-	std::cout << "B2 " << std::endl;
+	cout << "B2 " << endl;
 	kB = true;
 	if (kB && kA)	dot = false;
 }
@@ -27,7 +30,7 @@ void dotta()
 {
 	while (dot)
 	{
-		std::cout << ". " << std::endl;
+		cout << ". " << endl;
 		std::chrono::milliseconds sec(500);
 		std::this_thread::sleep_for(sec);
 	}
@@ -39,13 +42,13 @@ int main()
 	std::thread A(funA);
 	std::thread B(funB);
 
-	std::cout << "Wykonuja sie" << std::endl;
+	cout << "Wykonuja sie" << endl;
 
 	dotter.join();
 	A.join();
 	B.join();
 
-	std::cout << "Skonczyly sie" << std::endl;
+	cout << "Skonczyly sie" << endl;
 
 	system("PAUSE");
 	return 0;
