2fb1d228285f00774a660411aaae30f3e5b0b4ea
Szop-Kradziej <karolina.drabarz@gmail.com>
2015-06-05 01:42:44 +0200

    SecureCommAction przerzuce do oddzielnego pliku

--- kontroler.hpp
+++ kontroler.hpp
@@ -8,18 +8,147 @@
 #include <sstream>
 #include <list>
 #include "model.hpp"
-#include "model.cpp"
+#include "widok.hpp"
+#include <map>
 
 using namespace std;
 
+class SecureCommAction
+	{
+		public:
+		SecureCommAction(){};
+		virtual void wykonajAkcje( Ekran* ekran){};
+		virtual ~SecureCommAction(){};
+	};
+	
+	class RejestrowanieAction : public SecureCommAction
+	{
+		Model * model;
+		
+		public:
+		RejestrowanieAction(Model* model)
+		{
+			this->model = model;
+		};
+		void wykonajAkcje( Ekran* ekran)
+		{
+			string login = ((EkranRejestracji*)ekran)->getLogin();
+			string haslo = ((EkranRejestracji*)ekran)->getHasloSciezka();
+			
+			ekran->wyswietlLinieTekstu("Czekaj na rejestracje.");
+			
+			model->wyslijKomunikatRejestracji(RegistrationRequest(login,haslo));
+		};
+		
+		~RejestrowanieAction(){};
+	};
+	
+	class AutoryzacjaHaslemAction : public SecureCommAction
+	{
+		Model *model;
+		//Ekran*ekran;
+		
+		public:
+		AutoryzacjaHaslemAction(Model *model)
+		{
+			this->model = model;
+			
+		};
+		void wykonajAkcje(Ekran* ekran)
+		{
+			string login = ((EkranAutoryzacjiHaslem*)ekran)->getLogin();
+			string haslo = ((EkranAutoryzacjiHaslem*)ekran)->getHasloSciezka();
+			
+			ekran->wyswietlLinieTekstu("Czekaj na autoryzacje haslem.");
+			
+			model->wyslijKomunikatLogowania(LoginRequest(login,haslo));
+		};
+		
+		~AutoryzacjaHaslemAction()
+		{
+			//delete model;
+			//delete ekran;
+		};
+	};
+	
+	class AutoryzacjaKluczemAction : public SecureCommAction
+	{
+		public:
+		AutoryzacjaKluczemAction()
+		{
+		
+		};
+		void wykonajAkcje( Ekran* ekran){};
+		
+		~AutoryzacjaKluczemAction(){};
+	};
+	
+	class KomunikacjaAction : public SecureCommAction
+	{
+		public:
+		KomunikacjaAction()
+		{
+		
+		};
+		void wykonajAkcje( Ekran* ekran){};
+		
+		~KomunikacjaAction(){};
+	};
+	
+	class DodanieZnajomegoAction : public SecureCommAction
+	{
+		Model *model;
+		
+		public:
+		DodanieZnajomegoAction(Model *model)
+		{
+			this->model = model;
+		};
+		void wykonajAkcje( Ekran* ekran)
+		{
+			string id = ((EkranDodawaniaKontaktu*)ekran)->getIdZnajomego();
+			
+			model->wyslijKomunikatDodaniaZnajomego(FriendAdd(id));
+		};
+		
+		~DodanieZnajomegoAction(){};
+	};
+	
+	class WyswietlenieZnajomychAction : public SecureCommAction
+	{
+		Model * model;
+		
+		public:
+		WyswietlenieZnajomychAction(Model *model)
+		{
+			this->model = model;
+		};
+		void wykonajAkcje( Ekran* ekran)
+		{
+			model->wyslijKomunikatWyswietlaniaZnajomych(FriendListRequest());
+		};
+		
+		~WyswietlenieZnajomychAction(){};
+	};
+
 class Kontroler
 {
+	
 	private:
+	map<string, SecureCommAction*> mapaAkcji;
+	map<string, SecureCommAction*>::iterator it;
+	Ekran *ekran_pop;
+	Ekran *ekran;
 	Model *model;
 	
 	public:
 	Kontroler();
+	~Kontroler();
+	
 	void petlaAplikacji();
+	void wypelnijMapeAkcji();
 };
+
+
 
 #endif
