883cde8645bc288dc0fbfc666dc5d436753cf59d
Szop-Kradziej <karolina.drabarz@gmail.com>
2015-06-08 02:06:43 +0200

    update kontroler.cpp

--- Klient/kontroler.cpp
+++ Klient/kontroler.cpp
@@ -1,38 +1,62 @@
 #include "kontroler.hpp"
-#include "widok.hpp"
 
 using namespace std;
 
 Kontroler::Kontroler()
 {
 	model = new Model();
+	wypelnijMapeAkcji();
+}
+
+void Kontroler::wypelnijMapeAkcji()
+{
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranPowitalny",new SecureCommAction()));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranLogowania",new SecureCommAction()));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranRejestracji",new RejestrowanieAction(model)));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranAutoryzacjiHaslem",new AutoryzacjaHaslemAction(model)));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranAutoryzacjiKluczem",new SecureCommAction()));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranPowrotu",new SecureCommAction()));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranWyboruZnajomego", new WyborZnajomegoAction(model)));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranKomunikacji",new KomunikacjaAction(model)));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranDodaniaKontaktu",new DodanieZnajomegoAction(model)));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranWyswietlaniaListyUzytkownikow",new WyswietlenieZnajomychAction(model)));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranPoprawnejAutoryzacji",new SecureCommAction()));
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranWyboruZnajomegoBezposrednia",new WyborZnajomegoBezposredniaAction(model)));	
+	mapaAkcji.insert(pair<string,SecureCommAction*>("EkranKomunikacjiBezposredniej",new KomunikacjaBezposredniaAction(model)));
 }
 
 void Kontroler::petlaAplikacji()
 {
-	Ekran *ekran_pop = new EkranPowitalny();
-	Ekran *ekran;
+	
+	ekran_pop = new Ekran();
+	ekran_pop->czyscEkran();
+	ekran_pop = new EkranPowitalny();
+	
 	int odp;
 	
 	while(true)
-	{
+	{	
 		ekran_pop->wyswietlTekst(ekran_pop->zwrocListeKomunikatow());
 		odp = ekran_pop->oczekujNaOdpowiedz();
-		if(odp==5)
+		
+		it = mapaAkcji.find(ekran_pop->getNazwaKlasy());
+
+		if (it == mapaAkcji.end()) 
 		{
-			ekran = ekran_pop->przejdzDoWybranegoEkranu(odp);
-			ekran -> oczekujNaOdpowiedz();
-			cout<< "Mam piatke"<<endl;
-			model->polaczZSerwerem();
-			model->wyslijWiadomoscPosrednio("login",((EkranKomunikacji*) ekran)->getKomunikat());
-			cout<<(((EkranKomunikacji*) ekran)->getKomunikat());
-			model->zakonczPolaczenieZSerwerem();
-			ekran_pop = new EkranLogowania();
-			ekran_pop->wyswietlTekst(ekran_pop->zwrocListeKomunikatow());
-			odp = ekran_pop->oczekujNaOdpowiedz();
+			cout<< "Upsss cos poszlo nie tak";
+		} 
+		else 
+		{	
+			SecureCommAction* akcja = it->second;
+			akcja->wykonajAkcje(ekran_pop);
 		}
+		
 		ekran = ekran_pop->przejdzDoWybranegoEkranu(odp);
-		ekran->czyscEkran();
 		ekran_pop = ekran;
 	}
+	
+}
+Kontroler::~Kontroler()
+{
+	delete model;
 }
