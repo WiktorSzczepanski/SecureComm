e40c4788583b2d202759a9aed1cd333f6cd0ff5b
Szop-Kradziej <karolina.drabarz@gmail.com>
2015-06-08 02:10:40 +0200

    Update model.cpp

--- Klient/model.cpp
+++ Klient/model.cpp
@@ -4,63 +4,30 @@ using namespace std;
 
 int Model::polaczZSerwerem()
 {
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    const char *host = nazwaSerwera.c_str();
-    string wiad;
-    
-    
-    socketSerwer = socket(AF_INET, SOCK_STREAM, 0);
-    // Sprawdzenie wartości deskryptora:
-    if (socketSerwer < 0) 
-    {
-        fprintf(stderr,"ERROR opening socket");
-	}
-    // Sprawdzenie czy podany host istnieje:    
-    server = gethostbyname(host);
-    if (server == NULL) 
-    {
-        fprintf(stderr,"ERROR, no such host\n");
-    }
-    
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(numerPortuSerwera);
-    if (connect(socketSerwer,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        fprintf(stderr,"ERROR connecting");
-    printf("Połączono z serwerem");
- 
+    return 0;
+}
+
+int Model::wyslijWiadomoscPosrednio(WiadomoscPosrednia msg)
+{
+    klientNode->sendMessage(nazwaSerwera, msg);
     
     return 0;
 }
 
-int Model::wyslijWiadomoscPosrednio(string login, string tresc)
+int Model::wyslijWiadomoscBezposrednio(WiadomoscPosrednia msg)
 {
-	const int MAX_BUFOR = 512;
-	int n;
-	char buffer[MAX_BUFOR];
-	bzero(buffer,MAX_BUFOR);
-    strcpy(buffer, tresc.c_str());
-    n = write(socketSerwer,buffer,strlen(buffer));
-    if (n < 0) 
-         fprintf(stderr,"ERROR writing to socket");
-    bzero(buffer,MAX_BUFOR);
-    n = read(socketSerwer,buffer,MAX_BUFOR - 1);
-    if (n < 0) 
-         fprintf(stderr,"ERROR reading from socket");
-    printf("%s\n",buffer);
+    klientNodeBezposrednio->sendMessage(adresOdbiorcy, msg);
     
     return 0;
-	
+}
+
+int Model::utworzPolaczenieBezposrednie()
+{
+	klientNodeBezposrednio = new KlientNode(numerPortuOdbiorcy,numerPortuNadawcy);
 }
 
 int Model::zakonczPolaczenieZSerwerem()
-{
-	close(socketSerwer);
-	
+{	
 	return 0;
 }
 
@@ -68,4 +35,74 @@ void Model::blad(const char *msg)
 {
   //  perror(msg);
   //  exit(0);
+}
+
+int Model::wyslijKomunikatLogowania(LoginRequest msg)
+{
+	klientNode->sendMessage(nazwaSerwera, msg);
+	return 0;
+}
+
+int Model::wyslijKomunikatRejestracji(RegistrationRequest msg)
+{
+	klientNode->sendMessage(nazwaSerwera, msg);
+	return 0;
+}
+
+int Model::wyslijKomunikatDodaniaZnajomego(FriendAdd msg)
+{
+	klientNode->sendMessage(nazwaSerwera, msg);
+	return 0;
+}
+
+int Model::wyslijKomunikatWyswietlaniaZnajomych(FriendListRequest msg)
+{
+	klientNode->sendMessage(nazwaSerwera, msg);
+	return 0;
+}
+
+void Model::dodajKomunikacjeZeZnajomym(string id)
+{
+	for (list<pair<string,string> >::iterator it = komunikacjeZeZnajomymi.begin(); it != komunikacjeZeZnajomymi.end(); it++)
+	{
+
+	}
+}
+
+void Model::dodajKomunikacjeBezposredniaZeZnajomym(string adres)
+{
+	for (list<pair<string,string> >::iterator it = komunikacjeBezposrednieZeZnajomymi.begin(); it != komunikacjeBezposrednieZeZnajomymi.end(); it++)
+	{
+
+	}
+}
+
+void Model::setLoginUzytkownika(string login)
+{
+	loginUzytkownika = login;
+}
+
+string Model::getLoginUzytkownika()
+{
+	return loginUzytkownika;
+}
+
+void Model::setIdOdbiorcy(string id)
+{
+	idOdbiorcy = id;
+}
+
+string Model::getIdOdbiorcy()
+{
+	return idOdbiorcy;
+}
+
+void Model::setAdresOdbiorcy(string adres)
+{
+	adresOdbiorcy = adres;
+}
+
+string Model::getAdresOdbiorcy()
+{
+	return adresOdbiorcy;
 }
