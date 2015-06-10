b55bafe588ed8694e7b9ea37c0fb490953891164
Szop-Kradziej <karolina.drabarz@gmail.com>
2015-06-10 14:56:33 +0200

    Update model.hpp

--- model.hpp
+++ model.hpp
@@ -13,43 +13,73 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <stdio.h>
-#include "Sender.h"
-#include "Listener.h"
-#include "CommunicationNode.h"
+#include "KlientNode.hpp"
 #include "Komunikaty/Czesc.h"
 #include "Komunikaty/LoginRequest.h"
 #include "Komunikaty/RegistrationRequest.h"
 #include "Komunikaty/FriendAdd.h"
 #include "Komunikaty/FriendListRequest.h"
-//#include "Komunikaty/LogOut.h"
-/*#include "Komunikaty/WiadomoscPosrednia.h"*/
-
+#include "Komunikaty/LogOut.h"
+#include "Komunikaty/WiadomoscPosrednia.h"
+#include "Komunikaty/FriendSeek.h"
 
 
 using namespace std;
 
+
+class SecureCommReaction
+{
+	public:
+	SecureCommReaction(){};
+	virtual void wykonajReakcje()
+	{
+			
+	};
+	virtual ~SecureCommReaction(){};
+};
+
+
 class Model
 {
 	private:
-	// Sender.
-	Sender sender = Sender(22222);
-	// Nazwa serwera.
-	const string nazwaSerwera = "localhost";
+	BQueue<string> *blockingQueue;
 	// Numer portu serwera.
 	const int numerPortuSerwera = 22222;
+	// Numer portu klienta.
+	const int numerPortuKlienta = 22233;
+	// Numer portu odbiorcy.
+	const int numerPortuOdbiorcy = 22244;
+	// Numer portu nadawcy.
+	const int numerPortuNadawcy = 22245;
+	// Wezel komunikacji
+	KlientNode* klientNode = new KlientNode(numerPortuSerwera, numerPortuKlienta,blockingQueue);
+	// Nazwa serwera.
+	const string nazwaSerwera = "localhost";
 	// Deskryptor gniazda połączenia z serwerem.
 	int socketSerwer;
 	// Deskryptory gniazd połączeń bezpośrednich.
-	list<int> socketUzytkownicy;
-	
+	list<pair<string,string> > komunikacjeZeZnajomymi;
+	// Deskryptory gniazd połączeń bezpośrednich.
+	list<pair<string,string> > komunikacjeBezposrednieZeZnajomymi;
+	KlientNode* klientNodeBezposrednio;
+	// Nazwa użytkownika.
+	string loginUzytkownika;
+	string idOdbiorcy;
+	string adresOdbiorcy;
 	
 	public:
+	Model(BQueue<string> *blockingQueue);
 	// Metoda służąca do wysyłania bezpośredniej wiadomości do 
 	// innego użytkownika.
-	int wyslijWiadomoscBezposrednio(string login, string tresc);
+	int wyslijWiadomoscBezposrednio(WiadomoscPosrednia msg);
+	int utworzPolaczenieBezposrednie();
+	// Dodaje komunikacje ze znajomym.
+	void dodajKomunikacjeZeZnajomym(string id);
+	// Dodaje komunikacje ze znajomym.
+	void dodajKomunikacjeBezposredniaZeZnajomym(string adres);
 	// Metoda służąca do wysyłania pośredniej wiadomości do innego
 	// użytkownika.
-	//int wyslijWiadomoscPosrednio(WiadomoscPosrednia msg);
+	int wyslijWiadomoscPosrednio(WiadomoscPosrednia msg);
 	// Metoda służąca do odbierania wiadomości od serwera.
 	int odbierzWiadomoscOdSerwera();
 	// Metoda służąca do wysłanie komunikatu logowania.
@@ -66,7 +96,16 @@ class Model
 	int zakonczPolaczenieZSerwerem();
 	// Funkcja zwracająca informację o błędzie.
 	void blad(const char *msg);
+	void setLoginUzytkownika(string login);
+	string getLoginUzytkownika();
+	void setIdOdbiorcy(string id);
+	string getIdOdbiorcy();
+	void setAdresOdbiorcy(string adres);
+	string getAdresOdbiorcy();
+	void dodajZdarzenieDoKolejki(string event);
 	
 };
+
+
 
 #endif
