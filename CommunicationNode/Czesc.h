61d9bb09870765563ea932e22e770512a39657b9
krepel <marius@crunchbang>
2015-06-10 14:59:06 +0200

    dodane przed prezentacja drobnostki

---
+++ CommunicationNode/Czesc.h
@@ -0,0 +1,23 @@
+#ifndef SECURECOMM_CZESC_H
+#define SECURECOMM_CZESC_H
+
+#include <string>
+#include "Komunikaty/Komunikat.h"
+
+class Czesc : public Komunikat
+{
+    private:
+    std::string wiad;
+
+    public:
+    Czesc(std::string w)
+   {
+	wiad = w;
+   };
+    std::string toString() const
+    {
+        return wiad;
+    }
+};
+
+#endif //SECURECOMM_CZESC_H
