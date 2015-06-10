1cd0953cbadd32d39d857ebf2f81ba9710302440
Szop-Kradziej <karolina.drabarz@gmail.com>
2015-06-10 14:55:05 +0200

    Update model.cpp

--- model.cpp
+++ model.cpp
@@ -2,17 +2,34 @@
 
 using namespace std;
 
+Model::Model(BQueue<string> *blockingQueue)
+{
+	this->blockingQueue = blockingQueue;
+}
+
 int Model::polaczZSerwerem()
 {
     return 0;
 }
-/*
+
 int Model::wyslijWiadomoscPosrednio(WiadomoscPosrednia msg)
 {
-    sender.connectionlessSend("localhost", msg);
+    klientNode->sendMessage(nazwaSerwera, msg);
     
     return 0;
-}*/
+}
+
+int Model::wyslijWiadomoscBezposrednio(WiadomoscPosrednia msg)
+{
+    klientNodeBezposrednio->sendMessage(adresOdbiorcy, msg);
+    
+    return 0;
+}
+
+int Model::utworzPolaczenieBezposrednie()
+{
+	klientNodeBezposrednio = new KlientNode(numerPortuOdbiorcy,numerPortuNadawcy,blockingQueue);
+}
 
 int Model::zakonczPolaczenieZSerwerem()
 {	
@@ -27,24 +44,75 @@ void Model::blad(const char *msg)
 
 int Model::wyslijKomunikatLogowania(LoginRequest msg)
 {
-	sender.connectionlessSend("localhost",msg);
+	klientNode->sendMessage(nazwaSerwera, msg);
 	return 0;
 }
 
 int Model::wyslijKomunikatRejestracji(RegistrationRequest msg)
 {
-	sender.connectionlessSend("localhost",msg);
+	klientNode->sendMessage(nazwaSerwera, msg);
 	return 0;
 }
 
 int Model::wyslijKomunikatDodaniaZnajomego(FriendAdd msg)
 {
-	sender.connectionlessSend("localhost",msg);
+	klientNode->sendMessage(nazwaSerwera, msg);
 	return 0;
 }
 
 int Model::wyslijKomunikatWyswietlaniaZnajomych(FriendListRequest msg)
 {
-	sender.connectionlessSend("localhost",msg);
+	klientNode->sendMessage(nazwaSerwera, msg);
 	return 0;
+}
+
+void Model::dodajKomunikacjeZeZnajomym(string id)
+{
+	for (list<pair<string,string> >::iterator it = komunikacjeZeZnajomymi.begin(); it != komunikacjeZeZnajomymi.end(); it++)
+	{
+
+	}
+}
+
+void Model::dodajKomunikacjeBezposredniaZeZnajomym(string adres)
+{
+	for (list<pair<string,string> >::iterator it = komunikacjeBezposrednieZeZnajomymi.begin(); it != komunikacjeBezposrednieZeZnajomymi.end(); it++)
+	{
+
+	}
+}
+
+void Model::setLoginUzytkownika(string login)
+{
+	loginUzytkownika = login;
+}
+
+string Model::getLoginUzytkownika()
+{
+	return loginUzytkownika;
+}
+
+void Model::setIdOdbiorcy(string id)
+{
+	idOdbiorcy = id;
+}
+
+string Model::getIdOdbiorcy()
+{
+	return idOdbiorcy;
+}
+
+void Model::setAdresOdbiorcy(string adres)
+{
+	adresOdbiorcy = adres;
+}
+
+string Model::getAdresOdbiorcy()
+{
+	return adresOdbiorcy;
+}
+
+void Model::dodajZdarzenieDoKolejki(string event)
+{
+	blockingQueue->push(event);
 }
